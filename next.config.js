309a6db0dfea3b5c764c905057bd9b2dfcaee6c6
Jeff15321 <jeff.lu234@gmail.com>
2024-11-05 00:11:28 -0500

    jeff wants to go to bed

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    // No need for experimental.appDir anymore
+  };
+  
+  module.exports = nextConfig;
+  
