aeac5e44f62cacf3ef2d23ecef0d2eaa4ab2b025
Jeff15321 <jeff.lu234@gmail.com>
2024-11-08 00:43:44 -0500

    fixed some terminal bugs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,8 @@ import Terminal from "../components/Terminal"; // Adjusted import path to match 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-6">
-      <div style={{position: "relative", width: "100%", height: "90vh"}}>
-        <Terminal height={50} width={70}/>
+      <div className="index-terminal-container">
+        <Terminal height={100} width={100}/>    
       </div>
     </main>
   );
