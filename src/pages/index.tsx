1b3a71e7922ad66e27b5b3e05baa455de7bdcb7e
Jeff15321 <jeff.lu234@gmail.com>
2024-11-09 00:14:08 -0500

    time to sleep

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import BentoPage from "../components/BentoPage";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-6">
-      <BentoPage projectName="PUT"/>
+      <BentoPage projectName="Time_Table_Sweetie"/>
     </main>
   );
 }
