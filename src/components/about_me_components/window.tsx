c9b84c6dedef111ac5acb1a909154a63b4df010d
Jeff15321 <jeff.lu234@gmail.com>
2024-11-13 20:49:56 -0500

    working windows

--- src/components/about_me_components/window.tsx
+++ src/components/about_me_components/window.tsx
@@ -1,9 +1,43 @@
 import React from "react";
 
-export default function Window() {
+interface WindowProps {
+    size: number;
+    height: number;
+    x: number;
+    y: number;
+}
+
+export default function Window({x, y, size, height}: WindowProps) {
+    const [isVisible, setIsVisible] = React.useState(true);
+    const [windowSize, setWindowSize] = React.useState(size);
+
+    const handleClose = () => {
+        setIsVisible(false);
+    };
+
+    const handleIncreaseSize = () => {
+        if (windowSize == 1.4) {
+            setWindowSize(1);
+        } else {
+            setWindowSize(1.4);
+        }
+    }
     return (
-        <div className="window-container">
-            <span>Hello</span>
+        <div className="window-container" style={{
+            visibility: isVisible ? 'visible' : 'hidden',
+            height: `${windowSize * 17}vw`,
+            width: `${height == 0 ? windowSize * 15 : windowSize * 17 * height}vw`,
+            top: `${50 + y}vh`, 
+            left: `${50 + x}vw`}}>
+            <div className="title-button-container">    
+                <button className="title-button" onClick={handleClose} style={{fontSize: `${windowSize * 0.9}em`}}>-</button>
+                <button className="title-button" onClick={handleIncreaseSize} style={{fontSize: `${windowSize * 0.9}em`}}>o</button>
+                <button className="title-button close-button" onClick={handleClose} style={{fontSize: `${windowSize * 0.9}em`}}>x</button>
+            </div>
+            <div className="index-image-container">
+
+            </div>
+
         </div>
     )
 }
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Window from "../components/about_me_components/window";
 import "../styles/bento.css";
-import "../styles/global.css";
+import "../styles/globals.css";
 import "../styles/about_me.css";
 
 export default function Test() {
     return (
-        <Window/>
+        <Window x={0} y={10} size={1} height={1.5}/>
     );
 }
