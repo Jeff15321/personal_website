5a15ec06f1d64e0462a7de48d63dd7463955f0c9
Jeff Lu <jeff.lu234@gmail.com>
2024-12-06 12:41:14 -0500

    fixed experience context

--- src/contexts/ExperienceContext.tsx
+++ src/contexts/ExperienceContext.tsx
@@ -17,7 +17,7 @@ export const ExperienceProvider: React.FC<{ children: React.ReactNode }> = ({ ch
         "UofT Formula Racing Team", 
         "Perception Build Team", 
         `
-• Transforming perception software from \\Python\\ into \\C++\\ and \\vectorized\\ serialized calculations to minimize computation time
+• Transforming perception software from \\Python\\ into \\C++\\ and vectorized serialized calculations to minimize computation time
 • Writing customer drivers using \\Python, C++, and Ros2\\ to integrate new hardware
 • Skilled in sensor integration, data analysis, and real-time systems.`
     ],
@@ -27,15 +27,15 @@ export const ExperienceProvider: React.FC<{ children: React.ReactNode }> = ({ ch
         "Guitar Teacher", 
         `
 • Worked \\15 hours\\ weekly and taught classical guitar from \\levels 1 to 8\\.
-• Wrote over \\30\\ easier guitar covers of popular music from student's requests`
+• Wrote over \\30\\ guitar covers and tutorialsof popular music from student's requests`
     ],
     [   
         "07/2023 - 04/2024",
         "International Public Health Internship", 
         "Workshop Team Lead", 
         `
-• Led a group of \\5 students\\ and conducted comprehensive research on prevalent health issues in Nigeria
-• Implemented \\3 workshops\\ related to Marasmus prevention that benefited over \\200 locals\\
+• Led a group of \\5 student researchers\\ and conducted comprehensive research on prevalent health issues in Nigeria relating to Marasmus
+• Implemented \\3 workshops\\ about Marasmus health education that benefited over \\140 locals\\
 • Conducted weekly meetings with local health instructors to organize events and learn about pressing local health concerns`
     ],
     [
@@ -43,7 +43,7 @@ export const ExperienceProvider: React.FC<{ children: React.ReactNode }> = ({ ch
         "Agincourt C.I.", 
         "DECA Chapter Founding President", 
         `
-• Led \\11\\ executives in planning and executing over \\10\\ school-wide workshops and events with an average of \\42\\ student attendance
+• Led \\11\\ executives in planning and executing \\9\\ school-wide workshops and events with an average of \\42\\ student attendance
 • Monitored project planning, tracked expenses, prepared financial reports
 • Fundraised over \\$3000\\ in club fund and charity donations`
     ]
