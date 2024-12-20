ac7752292161fa96abdc74f5128c2420fbdea502
Jeff Lu <jeff.lu234@gmail.com>
2024-12-20 17:30:09 -0500

    changed background blocking

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,9 @@ import BentoPage from "../components/BentoPage";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-6">
-      <BentoPage projectName="Time_Table_Sweetie"/>
+    <>
       <AboutMe/>
-    </main>
+      <BentoPage projectName="Time_Table_Sweetie"/>
+    </>
   );
 }
