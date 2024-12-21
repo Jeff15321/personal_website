792023cc2ac47c94996e693f37c583d5f9606772
Jeff Lu <jeff.lu234@gmail.com>
2024-12-21 00:56:07 -0500

    added photos

--- src/contexts/AboutMeContext.tsx
+++ src/contexts/AboutMeContext.tsx
@@ -13,42 +13,67 @@ export const AboutMeProvider: React.FC<{ children: React.ReactNode }> = ({ child
     const [aboutMe] = useState<Record<number, Record<number, Record<string, string>>>>({
         1: {
             1: {
-                title: "TEXT",
-                description: "Throughout my academic journey, I've participated in numerous programming competitions and hackathons",
-                link: "project1/cat1.jpg"  
+                title: "2024-09",
+                description: "3rd Place General Award at Hack the Hill II",
+                link: "about-me/competitions/hth.JPG"  
             },
             2: {
-                title: "TEXT", 
-                description: "Throughout my academic journey, I've participated in numerous programming competitions and hackathons",
-                link: "project1/cat1.jpg"
+                title: "2024-09", 
+                description: "3rd Place General Award at Hack the Valley 9",
+                link: "about-me/competitions/htv.PNG"
             },
             3: {
-                title: "TEXT",
-                description: "Throughout my academic journey, I've participated in numerous programming competitions and hackathons", 
-                link: "project1/cat1.jpg"
+                title: "2024-10",
+                description: "1st Place General Award at New Hacks", 
+                link: "about-me/competitions/newhacks.jpg"
             },
             4: {
-                title: "TEXT",
-                description: "Throughout my academic journey, I've participated in numerous programming competitions and hackathons",
-                link: "project1/cat1.jpg"
+                title: "2024-04",
+                description: "Led our DECA chapter to its first competition as the founding president",
+                link: "about-me/competitions/deca.JPG"
             },
             5: {
-                title: "TEXT",
-                description: "Throughout my academic journey, I've participated in numerous programming competitions and hackathons",
-                link: "project1/cat1.jpg"
+                title: "2023-06",
+                description: "2nd Place Nationally in HOSA and competed internationally in Texas HOSA finals",
+                link: "about-me/competitions/hosa.JPG"
+            },
+            6: {
+                title: "2023-02",
+                description: "Represented empathy(murdered) and apathy(reincarnated as) in the UTMUN Inside Out Conference",
+                link: "about-me/competitions/utmun.JPG"
             },
     
         },
         2: {
             1: {
-                title: "TEXT",
-                description: "Throughout my academic journey, I've participated in numerous programming competitions and hackathons",
-                link: "project1/cat1.jpg"  
+                title: "2024-10",
+                description: "TUBA(University of Toronto Open Ultimate Frisbee) at 2024 CUUC Nationals",
+                link: "about-me/sports/tuba3.JPG"  
             },
             2: {
-                title: "TEXT",
-                description: "Throughout my academic journey, I've participated in numerous programming competitions and hackathons",
-                link: "project1/cat1.jpg"  
+                title: "2024-09",
+                description: "University of Toronto Formula Racing Team - Driverless Perception Team Member",
+                link: "about-me/sports/utfr.jpg"  
+            },
+            3: {
+                title: "2023-10",
+                description: "Board 3 on ACI chess team",
+                link: "about-me/sports/chess1.jpeg"  
+            },
+            4: {
+                title: "2024-10",
+                description: "TUBA when jersies finally arrived",
+                link: "about-me/sports/tuba2.jpg"  
+            },
+            5: {
+                title: "2023-09",
+                description: "2nd Place at 2023 Birchmount Chess Tournament",
+                link: "about-me/sports/chess2.jpeg"  
+            },
+            6: {
+                title: "2024-02",
+                description: "3rd Place at 2024 Birchmount Chess Tournament",
+                link: "about-me/sports/chess3.jpeg"
             },
         },
         3: {
@@ -60,9 +85,29 @@ export const AboutMeProvider: React.FC<{ children: React.ReactNode }> = ({ child
         },
         4: {
             1: {
-                title: "TEXT",
-                description: "Throughout my academic journey, I've participated in numerous programming competitions and hackathons",
-                link: "project1/cat1.jpg"  
+                title: "ROCKY",
+                description: "I love my cat",
+                link: "about-me/rocky/rocky1.JPG"  
+            },
+            2: {
+                title: "ROCKY",
+                description: "I love my cat",
+                link: "about-me/rocky/rocky3.JPG"   
+            },
+            3: {
+                title: "ROCKY",
+                description: "I love my cat",
+                link: "about-me/rocky/rocky4.JPG"  
+            },
+            4: {
+                title: "ROCKY",
+                description: "I love my cat",
+                link: "about-me/rocky/rocky5.JPG"  
+            },
+            5: {
+                title: "CODY",
+                description: "I love my dog (occasionally)",
+                link: "about-me/rocky/cody1.JPG"  
             },
         },
         5: {
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from "react";
 
 import PopUpWindows from "../components/about_me_components/PopUpWindows";
+import HomePageButton from "../components/about_me_components/HomePageButton";
+import VerticleTrack from "../components/about_me_components/VerticleTrack";
+import HorizontalTrack from "../components/about_me_components/HorizontalTrack";
+import HorizontalToVerticleAnimation from "../components/about_me_components/HorizontalToVerticleAnimation";
+import AboutMe from "../components/about_me_components/AboutMe";
 
 export default function Test() {
-
+    const [ImageIndex, setImageIndex] = useState(1);
     return (
         <>
-        <PopUpWindows />
+         <AboutMe/>
         </>
     )};
